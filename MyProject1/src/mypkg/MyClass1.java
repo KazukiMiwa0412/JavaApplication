56d008ec84a80109c68b07f318525d625c66503f
KazukiMiwa0412 <63925314+KazukiMiwa0412@users.noreply.github.com>
2022-03-09 23:51:59 +0900

    first commit.

---
+++ MyProject1/src/mypkg/MyClass1.java
@@ -0,0 +1,17 @@
+package mypkg;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MyClass1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		List<String> list = Arrays.asList(args);
+
+		for (String object : list) {
+			System.out.println(object);
+		}
+	}
+
+}
